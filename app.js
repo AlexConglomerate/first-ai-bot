a84be8b243defa536a3a5d7f083adf4e9e50bae3
Alex <98337207+AlexConglomerate@users.noreply.github.com>
2023-03-01 18:53:24 +0600

    Убрал chatGPT

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
 const chalk = require('chalk')
 require('dotenv').config();
-const {generateAnswer} = require(`./chatGPT`)
+// const {generateAnswer} = require(`./chatGPT`)
 
-const token = process.env.BOT_TOKEN;
+// const token = process.env.BOT_TOKEN;
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot('6265339648:AAHcApPExWp8zhKvlIFOleVGvCl6mXxFxD0', {polling: true});
 
 const start = async () => {
     await bot.setMyCommands([
@@ -21,9 +21,9 @@ const start = async () => {
         }
 
 
-        const text = await generateAnswer(message)
+        // const text = await generateAnswer(message)
 
-        return bot.sendMessage(chatId, text);
+        return bot.sendMessage(chatId, "text");
     })
 
     const text = chalk.bgGreen(`             `) + chalk.green(` Telegram bot is running  => `) + chalk.green(new Date().toLocaleTimeString())
